0e51e373cb7ea0180ff63ed46ea9d11f4549ff83
doridoridoridorin <156412698+doridoridoridorin@users.noreply.github.com>
2026-06-30 23:26:24 +0900

    feat: カタログでお絵描きスレを緑枠線強調ではなくアイコン表示に変更

---
+++ src/shared/ui/media/OekakiBadge.tsx
@@ -0,0 +1,13 @@
+import iconOekaki from "@/assets/img/icon_oekaki.svg";
+
+/**
+ * お絵描きスレのサムネイル右上に表示する小さなバッジ
+ * カタログなどのグリッド表示で使用
+ */
+export const OekakiBadge: React.FunctionComponent = () => {
+  return (
+    <div className="absolute top-1 right-1 p-1 bg-black/50 rounded">
+      <img src={iconOekaki} alt="" aria-hidden="true" className="w-4 h-4" />
+    </div>
+  );
+};
